f2acca1e5805a383633562b2baca335bbc1201ce
guptakaran-01 <guptakaranmanoj@amityonline.com>
2025-11-22 13:13:52 +0530

    Applied SOLID  Priciples

--- Account.h
+++ Account.h
@@ -1,32 +1,40 @@
 #ifndef Account_H
 #define Account_H
-#include "Transaction.h"
-#include <iostream>
-#include <vector>
-using namespace std;
 
-class Account
+#include "Transaction.h"
+#include "IAccount.h"
+
+#include <string>
+#include <vector>
+
+class Account : public IAccount
 {
 private:
     int Account_no;
-    string account_holder_name;
+    std::string account_holder_name;
     double balance;
-    vector<Transaction> transactions;
+    std::vector<Transaction> transactions;
 
 public:
-    Account(int Account_no, string name, double balance);
-    virtual ~Account();
-    void setname(string name);
-    int get_Account_No();
-    string get_account_holder_name();
-    double get_balance();
-    bool deposit(double amount);
-    virtual bool withdraw(double amount);
+    Account(int Account_no, const std::string &name, double balance);
+    virtual ~Account() = default;
+    void setname(const std::string &name);
+
+    int get_Account_No() const override;
+    std::string get_account_holder_name() const override;
+    double get_balance() const override;
+
+    bool deposit(double amount) override;
+    virtual bool withdraw(double amount) override = 0;
+
+    void displayBalance() const;
+
+    void print_transaction_history() const override;
+    void exportHistory(const std::string &filename) const override;
+
+protected:
     void change_balance(double amount);
-    void displayBalance();
-    void record_transaction(string type, double amount);
-    void print_transaction_history();
-    void exportHistory(const std::string& filename) const;
+    void record_transaction(const std::string &type, double amount);
 };
 
 class Saving_Account : public Account
@@ -35,7 +43,7 @@ private:
     double interest_rate;
 
 public:
-    Saving_Account(int Account_no, string name, double balance, double rate);
+    Saving_Account(int Account_no, const std::string &name, double balance, double rate);
     bool addInterest();
 };
 
@@ -45,7 +53,7 @@ private:
     double over_draft_limit;
 
 public:
-    Current_Account(int Account_no, string name, double balance, double overdraft_limit);
+    Current_Account(int Account_no, const std::string &name, double balance, double overdraft_limit);
     bool withdraw(double amount) override;
 };
 
